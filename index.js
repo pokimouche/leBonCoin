5e6a5e95bda7579f95c25dbf441890253f9b9e3e
olivier poccard <olivierpoccard@mbp-de-olivier.lan>
2020-01-23 15:54:41 +0100

    new commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const formidableMiddleware = require("express-formidable");
-
+require("dotenv").config();
 const app = express();
 app.use(formidableMiddleware());
-
-mongoose.connect("mongodb://localhost/leBonCoin", {
+console.log(process.env.MONGODB_URI, "bonjour");
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true
@@ -15,4 +15,4 @@ const userRoutes = require("./routes/user");
 const offerRoutes = require("./routes/offer");
 app.use(userRoutes);
 app.use(offerRoutes);
-app.listen(3000, () => console.log("Server started"));
+app.listen(process.env.PORT, () => console.log("Server started"));
